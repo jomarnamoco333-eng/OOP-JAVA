1efa8024710378eee546538058a942d0f1337eda
jomarnamoco333-eng <jomarnamoco333@gmail.com>
2025-08-20 23:28:42 +0800

    Add files via upload

---
+++ javaapplication1/Problem3.java
@@ -0,0 +1,14 @@
+public class Problem3 {
+
+    public static void main(String[] args) {
+        // Declare two integer variables
+        int number1 = 21;
+        int number2 = 3;
+        
+        // Perform division and store the result in a variable
+        int result = number1 / number2;
+        
+        // Print the result to the console
+        System.out.println("The result of the division is: " + result);
+    }
+}
